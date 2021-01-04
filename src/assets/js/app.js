6a1dace7a7bc9a254b2aeb97b6eeef3d4267f482
Akarys <aka.shorabek01@gmail.com>
2021-01-04 12:25:59 +0600

    add: main-page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,26 @@
-console.log(1);
+$(document).ready(function() {
+    $(".set > a").on("click", function(event) {
+        event.preventDefault();
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".content")
+          .slideUp(200);
+        $(".set > a i")
+          .removeClass("active")
+      } else {
+        $(".set > a i")
+          .removeClass("active")
+        $(this)
+          .find("i")
+          .addClass("active");
+        $(".set > a").removeClass("active");
+        $(this).addClass("active");
+        $(".content").slideUp(200);
+        $(this)
+          .siblings(".content")
+          .slideDown(200);
+      }
+    });
+  });
+  
